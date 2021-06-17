2447335e10e3c0e1df500e4ef947522cfd6db738
hado anselme <anselmehado85@yahoo.com>
2021-06-18 00:18:46 +0100

    commmet

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,7 +1,19 @@
-FactoryBot.define do
-  factory :task do
-    # Please change the following contents according to the column name actually created
-    name { 'undefined_title' }
-    content { 'undefined_content' }
-  end
-end
+# FactoryBot.define do
+#   factory :task do
+#     # Please change the following contents according to the column name actually created
+#     name { 'undefined_title' }
+#     content { 'undefined_content' }
+#   end
+
+  # factory :second_task do
+  #   # Please change the following contents according to the column name actually created
+  #   name { 'undefined_title' }
+  #   content { 'undefined_content' }
+  # end
+  #
+  # factory :second_task do
+  #   # Please change the following contents according to the column name actually created
+  #   name { 'undefined_title' }
+  #   content { 'undefined_content' }
+  # end
+# end
--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -1,36 +1,31 @@
 require 'rails_helper'
 RSpec.describe 'Task management function', type: :system do
-
-  before do
-
-  FactoryBot.create(:task)
-  FactoryBot.create(:second_task)
+  describe 'New creation function' do
+    context 'When creating a new task' do
+      it 'Should display created task' do
+        visit new_task_path
+        fill_in 'Name', with: 'Task1'
+        fill_in 'Content', with: 'details'
+        click_button 'Create Task'
+        expect(page).to have_content 'Task was successfully created.'
+      end
+    end
   end
-
   describe 'List display function' do
     context 'When transitioning to the list screen' do
       it 'The created task list is displayed' do
-
-        task = FactoryBot.create(:task, name: 'task')
-        visit tasks_path
-        expect(page).to have_content 'task'
-
       end
     end
-
-
-    context 'Task arranged' do
-      it 'New at the top' do
-      task1 =  FactoryBot.create(:task, name: 'task1')
-      task2 =  FactoryBot.create(:task, name: 'task2')
-      visit tasks_path
-      task_list = all('.task_row')
-      expect(page).to have_content 'task2'
-      expect(page).to have_content 'task1'
-      end
-    end
-
-
-
+  end
+  describe 'Detailed display function' do
+     context 'When transitioned to any task details screen' do
+       it 'The content of the relevant task is displayed' do
+          task = Task.create(name: 'task1', content: 'content1')
+         #task = Task.create(:task, name: 'task1', content: 'content1')
+    
+         visit tasks_path(task)
+         expect(page).to have_content 'task1'
+       end
+     end
   end
 end
